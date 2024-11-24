92717e1f91a126c8e65758e7a91fe86e35eaa330
Daniel Nachun <daniel.nachun@gmail.com>
2024-11-24 14:30:10 -0800

    try to fix build

--- recipe/build.sh
+++ recipe/build.sh
@@ -14,7 +14,7 @@ cargo-bundle-licenses \
 # build statically linked binary with Rust
 export RUSTONIG_DYNAMIC_LIBONIG=1
 export RUSTONIG_SYSTEM_LIBONIG=1
-export PKG_CONFIG_SYSROOT_DIR=${CONDA_BUILD_SYSROOT}
+export PKG_CONFIG_SYSROOT_DIR=${PREFIX}
 export PKG_CONFIG_PATH=${BUILD_PREFIX}/bin/pkg-config
 cargo install --bins --features oniguruma --no-track --locked --root ${PREFIX} --path .
 
